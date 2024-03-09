881dd0b14cd9a69d8a4091fbe67582d2b22af26c
Botheet <stown.two.feet@gmail.com>
2024-03-10 08:39:04 +0900

    MUIマテリアルをインストール。theme.tsを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
+import { IconButton, Typography } from "@mui/material";
+import AlarmIcon from "@mui/icons-material/Alarm";
+
 export default function Home() {
-	return(
-	<>
-		<h1>helloworld</h1>
-		<p>helloworld</p>
-	</>
-	)
+	const num: number = 1;
+	const num2: number = 2;
+	const total: number = num + num2;
+
+	return (
+		<>
+			<h1>helloworld</h1>
+			<p>helloworld</p>
+			<IconButton color="secondary" aria-label="add an alarm">
+				<AlarmIcon />
+			</IconButton>
+			<Typography variant="h4" gutterBottom>
+				{total}
+			</Typography>
+		</>
+	);
 }
-	
---
+++ src/styles/theme.ts
@@ -0,0 +1,43 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+	palette: {
+		primary: {
+			main: "#fff9cc",
+			contrastText: "#4d4d4d"
+		},
+		secondary: {
+			main: "#ffa74f",
+			contrastText: "#ffffff"
+		},
+		error: {
+			main: "#dc143c"
+		},
+		info: {
+			main: "#00bfff"
+		}
+		// text: {
+		// 	primary:
+		// },
+		// warning: {
+		// 	main:
+		// },
+		// darkPurple: {
+		// 	main:
+		// },
+		// lightPurple: {
+		// 	main:
+		// },
+		// whitePurple: {
+		// 	main:
+		// 	contrastText:
+		// },
+		// registrationPink: {
+		// 	main:
+		// 	contrastText:
+		// }
+	}
+	// typography: {
+	// 	fontFamily: "Inter, sans-serif"
+	// }
+});
