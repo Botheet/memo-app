5840bb2a38fa1ffa85414b29f84e2b4bdc2713e7
Botheet <stown.two.feet@gmail.com>
2024-04-27 09:26:31 +0900

    バリデーションの追加。

--- src/components/core/Button/index.tsx
+++ src/components/core/Button/index.tsx
@@ -2,7 +2,6 @@ import Button, { ButtonProps, ButtonPropsColorOverrides } from "@mui/material/Bu
 import { FC, ReactNode } from "react";
 
 type PrimaryButtonProps = ButtonProps & {
-	onClickButton: (number: number) => void;
 	buttonText: string;
 	isDisabled?: boolean;
 	index?: number | null;
@@ -11,23 +10,11 @@ type PrimaryButtonProps = ButtonProps & {
 };
 
 export const PrimaryButton: FC<PrimaryButtonProps> = (props) => {
-	const {
-		onClickButton,
-		buttonText,
-		isDisabled = false,
-		index = null,
-		size,
-		icon,
-		color = "primary",
-		...other
-	} = props; //分割代入。{}内をpropsで展開
+	const { buttonText, isDisabled = false, index = null, size, icon, color = "primary", ...other } = props; //分割代入。{}内をpropsで展開
 	return (
 		<>
 			<Button
 				{...other}
-				onClick={() => {
-					onClickButton(index !== null && index);
-				}} //indexがぬるじゃなかったら引数がここに入る
 				variant="contained"
 				size={size}
 				disabled={isDisabled}
