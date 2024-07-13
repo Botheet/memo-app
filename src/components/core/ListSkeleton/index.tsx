765de11bfa5c95dd6340050f6ab04f967e29ecc3
Botheet <stown.two.feet@gmail.com>
2024-07-14 08:40:55 +0900

    メモをロード中のスケルトンを移換

---
+++ src/components/core/ListSkeleton/index.tsx
@@ -0,0 +1,26 @@
+import { Skeleton } from "@mui/material";
+
+export const LoadingList = () => {
+	return (
+		<>
+			<Skeleton width="60%" />
+			<Skeleton width="60%" />
+			<Skeleton width="60%" />
+			<Skeleton width="60%" />
+			<Skeleton width="60%" />
+			<Skeleton width="60%" />
+			<Skeleton width="60%" />
+			<Skeleton width="60%" />
+			<Skeleton width="60%" />
+			<Skeleton width="60%" />
+			<Skeleton width="60%" />
+			<Skeleton width="60%" />
+			<Skeleton width="60%" />
+			<Skeleton width="60%" />
+			<Skeleton width="60%" />
+			<Skeleton width="60%" />
+			<Skeleton width="60%" />
+			<Skeleton width="60%" />
+		</>
+	);
+};
